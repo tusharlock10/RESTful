ea750c7cb546722d2078a970debeab5a5cd3ed2a
Tushar Jain <tusharlock10@gmail.com>
2019-08-17 17:16:49 +0530

    Update settings.py

--- RESTful/settings.py
+++ RESTful/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ess_=ny%ni+s%vp(7u%zs@hr-h*g#*g&vjorwctxn_giqcabt4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.137.1', '0.0.0.0', 'localhost']
 
 
 # Application definition
